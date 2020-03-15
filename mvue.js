d828b575540da0ffa40cd256febeda68f03ccb31
Dylan Liu <dylan.liu237@gmail.com>
2020-03-15 11:38:24 +0800

    add: Compile - 基本模板編譯邏輯

--- mvue.js
+++ mvue.js
@@ -2,12 +2,37 @@ class Compile {
   constructor(el, vm) {
     this.el = this.isElementNode(el) ? el : document.querySelector(el)
     this.vm = vm
-    // 1. 獲取文檔碎片，放入記憶體，減少頁面重繪造成的效能瓶頸
+    // 1. 將根元素內所有元素放入fragement(記憶體)，減少頁面重繪造成的效能瓶頸
     const fragment = this.node2Fragment(this.el)
     // 2. 編譯模板，directive (v-???) 和 {{ }}
-    
+    this.compile(fragment)
     // 3. 將編譯完成的fragment，插入到綁定的根元素上
     this.el.appendChild(fragment)
+  }
+  compile(fragment) {
+    // 1. 獲取子元素
+    const childNodes = fragment.childNodes
+    Array.from(childNodes).forEach( child => {
+
+      if (this.isElementNode(child)) {
+        // 是元素
+        this.compileElement(child)
+      } else {
+        // 是 {{}}
+        this.compileText(child)
+      }
+
+      // 如果元素中還有子元素、再進行遞歸編譯
+      if (child.childNodes && child.childNodes.length) {
+        this.compile(child)
+      }
+    })
+  }
+  compileElement(node) {
+    
+  }
+  compileText(node) {
+
   }
   node2Fragment (el) {
     let fragment = document.createDocumentFragment()
@@ -17,6 +42,7 @@ class Compile {
     }
     return fragment
   }
+  // 判斷是鬍鬚還是元素
   isElementNode (node) {
     return node.nodeType === 1
   }
