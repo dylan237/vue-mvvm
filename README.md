d7ad101ce925a129da37c1254eada638594c5609
Dylan Liu <dylan.liu237@gmail.com>
2020-03-15 11:00:51 +0800

    add: 基本結構

---
+++ README.md
@@ -0,0 +1,2 @@
+1. 實現 observe 觀察者
+2. 實現 compile 模板編譯器
---
+++ mvue.js
@@ -0,0 +1,14 @@
+class Dylan {
+  constructor(options) {
+    this.$el = options.el;
+    this.$data = options.data;
+    this.$options = options;
+
+    if (this.$el) {
+      // 1. 實現一個觀察者 observe
+
+      // 2. 實現一個指令解析器 compiler
+    }
+  }
+  
+}
